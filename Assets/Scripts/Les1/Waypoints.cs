7c7f66a3f5140a05f68ba5474446b899b92c2575
Minoord <minoord18@outlook.com>
2021-09-24 15:22:59 +0200

    Added enemy that goes to the waypoints

---
+++ Assets/Scripts/Les1/Waypoints.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints: MonoBehaviour
+{
+    
+    public Vector3 GetPosition()
+    {
+        return transform.position;
+    }
+
+}
---
+++ Assets/Scripts/Les2/PathFollower.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollower : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _arrivalTreshold;
+    private Path _path;
+    private Waypoints _currentWaypoint;
+
+    private void Start()
+    {
+        SetUpPath();
+    }
+    private void SetUpPath()
+    {
+        _path = FindObjectOfType<Path>();
+        _currentWaypoint = _path.GetPathStart();
+        transform.LookAt(_currentWaypoint.GetPosition());
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+
+        float distanceToWaypoint = Vector3.Distance(transform.position, _currentWaypoint.GetPosition());
+        if (distanceToWaypoint <= _arrivalTreshold)
+        {
+            if(_currentWaypoint == _path.GetPathEnd())
+            {
+                PathComplete();
+            }
+            else
+            {
+                _currentWaypoint = _path.GetNextWaypoint(_currentWaypoint);
+                transform.LookAt(_currentWaypoint.GetPosition());
+            }
+        }
+
+    }
+    private void PathComplete()
+    {
+        _speed = 0;
+    }
+}
